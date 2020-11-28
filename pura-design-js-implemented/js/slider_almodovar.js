cb101effba62548a28359668148c547b2559a6f9
Michal Nielubszyc <m.nielubszyc@gmail.com>
2020-11-28 17:12:33 +0100

    slider, slideshow and tab container finished

--- pura-design-js-implemented/js/slider_almodovar.js
+++ pura-design-js-implemented/js/slider_almodovar.js
@@ -1,17 +1,12 @@
 const slideContainer = document.querySelector('.gallery');
-const slideImages = document.querySelectorAll('.gallery div');
+const slideImages = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
 
 let counter = 1;
 const imageSize = slideImages[0].clientWidth;
-console.log(imageSize)
 
-// slideImages[1].clientWidth = imageSize; 
-// slideImages[2].clientWidth = imageSize; 
-// slideImages[3].clientWidth = imageSize; 
-// slideImages[4].clientWidth = imageSize; 
-
+// slideContainer.style.transform = 'translateX(-${counter ? 100*counter: 100}%)';
 
 slideContainer.style.transform = 'translateX(' + (-imageSize * counter) + 'px)';
 
@@ -19,28 +14,31 @@ nextBtn.addEventListener('click', ()=>{
     slideContainer.style.transition = 'transform 0.4s ease-in-out';
     counter++;
     slideContainer.style.transform = 'translateX(' + (-imageSize * counter) + 'px)';
+    console.log(counter)
 })
 
 prevBtn.addEventListener('click', ()=>{
     slideContainer.style.transition = 'transform 0.4s ease-in-out';
     counter--;
     slideContainer.style.transform = 'translateX(' + (-imageSize * counter) + 'px)';
+    console.log(counter)
+    console.log(slideImages[counter].id)
 })
 
-console.log(slideImages[0].clientWidth);
-console.log(slideImages[1].clientWidth);
-console.log(slideImages[2].clientWidth);
-console.log(slideImages[3].clientWidth);
-console.log(slideImages[4].clientWidth);
-console.log(slideImages[5].clientWidth);
-console.log(slideImages[6].clientWidth);
-console.log(slideImages[7].clientWidth);
-console.log(slideImages[8].clientWidth);
-console.log(slideImages[9].clientWidth);
-console.log(slideImages[10].clientWidth);
-console.log(slideImages[11].clientWidth);
-console.log(slideImages[12].clientWidth);
-console.log(slideImages[13].clientWidth);
-console.log(slideImages[14].clientWidth);
-console.log(slideImages[15].clientWidth);
-console.log(slideImages[16].clientWidth);
+// making slides go in a kind of loop from last to the first one
+
+slideContainer.addEventListener('transitionend', () =>{
+    if (slideImages[counter].id === 'lastClone'){
+        slideContainer.style.transition = 'none';
+        counter = slideImages.length -2;
+        slideContainer.style.transform = 'translateX(' + (-imageSize * counter) + 'px)';
+    }
+    if (slideImages[counter].id === 'firstClone'){
+        console.log('joł')
+        slideContainer.style.transition = 'none';
+        // counter = slideImages.length - counter;
+        counter = 1;
+        slideContainer.style.transform = 'translateX(' + (-imageSize * counter) + 'px)';
+    }
+})
+
