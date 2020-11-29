6a2dad330fc39093a9fb65f0dbba23ea40e616b5
Michal Nielubszyc <m.nielubszyc@gmail.com>
2020-11-29 20:41:42 +0100

    calculator: first operand and operator ready

--- calculator/calculator-app.js
+++ calculator/calculator-app.js
@@ -1,5 +1,4 @@
-// Calculator with operands and operator as an object.
-// Podstawowym zagadnieniem jest stworzenie obiektu calculator, w którym zapisujemy wszystkie wyklikane liczby i operacje. 
+// Podstawowym zagadnieniem jest stworzenie obiektu calculator, w którym zapisujemy wszystkie wyklikane liczby i operacje. Obiekt posiada parametry jak operands i operator oraz screenValue
 
 const calculator = {
     screenValue: '0',
@@ -8,7 +7,7 @@ const calculator = {
     operator: null
 };
 
-// Funkcja, która dodaje cyfry w screenValue w zależności czy dotychczas było tam 0 czy już jakaś cyfra. 
+// Funkcja, która dodaje cyfry w screenValue w zależności czy dotychczas było tam 0 czy już jakaś cyfra. Nie potrafię obsługiwać wyrażeń z '?' dlatego wykonuję ze zwykłym wyrażeniem 'if else'
 
 function addNumber(number){
     if(calculator.screenValue === '0'){
@@ -16,6 +15,7 @@ function addNumber(number){
     }else{
         calculator.screenValue = calculator.screenValue + number;
     }
+    console.log(calculator);
 }
 
 // Funkcja dodająca separator dziesiętny. Sprawdza czy obiekt calculator posiada już element dot, jeśli nie, to dodaje go.
@@ -27,14 +27,34 @@ function addDecimal(dot){
     }
 }
 
-// Updating the display
+
+// Funkcja, która obsługuje kliknięcia przycisków funkcyjnych (operatorów)
+
+// 1 przypadek - naciskamy operator po wprowadzeniu pierwszej liczby (firstOperand)
+
+function handleOperator(nextOperator){
+    // destructure propsów obiektu calculator
+    const { firstOperand, screenValue, operator} = calculator;
+    // za pomocą parseFloat zmieniamy stringa przechowywanego w calculator.screenValue w liczbę z miejscami po przecinku
+    const inputValue = parseFloat(screenValue);
+    //sprawdzenie czy firstOperand jest null (powinien być jeśli to pierwsze działanie) oraz czy inputValue jest numberem
+    if (firstOperand === null && !isNaN(inputValue)){
+        calculator.firstOperand = inputValue;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+
+    console.log(calculator);
+}
+
+// Funkcja służy do przerzucenia na display kalkulatora wartości screenValue, którą pobieram z obiektu calculator, do którego trafia ona z kolei na podstawie event.targetów 
 
 function updateScreen(){
     const screen = document.querySelector('.screen');
     screen.innerHTML = calculator.screenValue;
 }
 
-// Event Listener for all calculator buttons, using event delegation
 // Generalnie event listener przypisujemy całemu kontenerowi i okresleniami warunkowymi (przy pomocy .matches, które łapie elementy o określonej klasie) przypisujemy konkretnym typom przycisków konkretne działania, różne dla każdego typu. Wartością każdego kliknięcia jest event.target.innerHTML
 
 const calculatorButtons = document.querySelector('.calculatorContainer');
@@ -46,7 +66,8 @@ calculatorButtons.addEventListener('click', (event) =>{
         return;
     }
     if (event.target.matches('.operator')){
-        return;
+        handleOperator(event.target.innerHTML);
+        updateScreen();
     }
     if (event.target.matches('.allClear')){
         return;
