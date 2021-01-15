ae9848bfab47f002aa5c9501be2212730ef5f587
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-01-15 21:18:40 +0100

    finished

--- snake-game/js/snake-game.js
+++ snake-game/js/snake-game.js
@@ -1,4 +1,4 @@
-// Modal & form and getting previous settings from localStorage
+// 01. Modal & form and saving & getting previous settings from localStorage
 
 const currentSettings = (JSON.parse(localStorage.getItem('SNAKE_DATA'))) || {} 
 
@@ -13,10 +13,9 @@ let SNAKE_SPEED = 0;
 let EXPANSION_RATE = 0;
 let WALLS_ON_OFF = 0;
 let score = 0;
+let state;
 const closeModal = (item) => item.classList.add('modal--hidden');
 const showModal = (item) => item.classList.remove('modal--hidden');
-
-let state;
 
 const getFormData = (e) => {
     e.preventDefault();
@@ -33,7 +32,12 @@ const getFormData = (e) => {
 
 form.addEventListener('submit', getFormData);
 
-// Initialize Firebase, render best scores & save score in Firestore
+function saveInLocalStorage(){
+    const settingsArray = {USER_NAME, SNAKE_SPEED, EXPANSION_RATE};
+    localStorage.setItem('SNAKE_DATA', JSON.stringify(settingsArray))
+}
+
+// 02. Initialize Firebase, render best scores & save score in Firestore
 
 firebase.initializeApp(firebaseConfig);
 
@@ -49,51 +53,35 @@ function saveinFirestore(){
 
 const scoresContainer = document.querySelector('.scores-section')
 let listExists = false
-
 let scoresArray;
 
 function renderPreviousScores(documents) {
-
     scoresArray = [];
-
     documents.forEach((document) => {
         const data = document.data();
         scoresArray.push(data)
     })
-    
     const topScores = scoresArray.sort((a,b) => {
         return b.score - a.score;
     })
-    
     const topTenScores = topScores.slice(0,10) 
-
     if (listExists){
             const previousList = document.querySelector('ol')
             previousList.remove()
         }
     const list = document.createElement('ol');
     let html = '';
-    
     topTenScores.forEach((document) => {
         html += `<li class="single-score">${document.username} : ${document.score}</li>`
     });
-        
     list.innerHTML = html;
     scoresContainer.appendChild(list)
     listExists = true;
 }
 
-// Save user data in localStorage 
-
-function saveInLocalStorage(){
-    const settingsArray = {USER_NAME, SNAKE_SPEED, EXPANSION_RATE};
-    localStorage.setItem('SNAKE_DATA', JSON.stringify(settingsArray))
-}
-
-// Render settings
+// 03. Render settings and score
 
 const userInfo = document.querySelector('.user-info');
-const gameTimeInfo = document.querySelector('.game-time-info');
 const scoreInfo = document.querySelector('.score-info');
 const snakeSpeedInfo = document.querySelector('.snake-speed-info');
 const snakeExpansionInfo = document.querySelector('.snake-expansion-info');
@@ -118,7 +106,7 @@ const renderScoreTime = () => {
     scoreInfo.innerHTML = `Your score: ${score}`;
 }
 
-// 00. GAME ELEMENTS & USER VARIABLES //////////////////////////////////////
+// 04. GAME ELEMENTS & USER VARIABLES //////////////////////////////////////
 // Ciało snake'a będzie reprezentowane jako array obiektów zawierających pozycje x i y, które potem odniesiemy do grida
 
 let snakeBody = [];
@@ -126,7 +114,7 @@ let victim = {};
 const gameBoard = document.querySelector('.game-board');
 let gameOver = false;
 
-// 01. GAME ENGINE //////////////////////////////////////////////////////////
+// 05. GAME ENGINE //////////////////////////////////////////////////////////
 // Funkcja poniżej warunkuje jak często następuje update i render całej gry, dwiema zasadniczymi zmiennymi są tu 'currentTime' i 'lastUpdateTime' i one są tu porównywane. currentTime jest zdefiniowany odgórnie, nastomiast lastUpdateTime zaczynamy od 0 a potem po każdym requestAnimationFrame przypisujemy mu wartość currentTime. Dzięki temu jesteśmy w stanie operować rożnicą czasu pomiędzy dwoma kolejnymi iteracjami i definiujemy to jako timeSinceLastUpdate=(currentTime-LastUpdateTime)/1000 żeby było w s, a nie w ms. Następnie możemy ustalić jak często nasze iteracje mają następować , czyli jak często będziemy updatować i renderować grę. Służy nam do tego stała SNAKE_SPEED [1/s], której wartość oznacza ilość iteracji na sekundę. Tworząc poniższy warunek if definiujemy co ile s gra ma być updatowana (np. SNAKE_SPEED = 0,2 [1/s]) => gra będzie updateowana tylko jeśli iteracje będą co najmniej co 5s, a jeśli = 5 => gra będzie updateowana 5 razy na sekundę. I tylko wtedy wywołujemy funkcje render i update. Niżej wywołuję requestAnimationFrame po raz pierwszy.
 
 const startModal = document.querySelector('.start-modal')
@@ -186,12 +174,12 @@ function update(){
 const resetBtn = document.querySelector('.reset-game-btn');
 
 function reset(){
-    // state = 'notStarted'
     state = 'reset';
     window.cancelAnimationFrame(gameEngine);
     score = 0;
     lastUpdateTime = 0
     renderScoreTime();
+    closeModal(startModal);
     showModal(formModal);
     gameOver = false;
     victimScore = 0;
@@ -199,8 +187,7 @@ function reset(){
 
 resetBtn.addEventListener('click', reset)
 
-
-// 02. RENDERING SNAKE /////////////////////////////////////////////////////
+// 06. RENDERING SNAKE /////////////////////////////////////////////////////
 // Dla każdego obiektu z tablicy snakeBody (dzięki metodzie forEach) tworzymy 1 div, któremu nadajemy pozycje w gridzie pobrane z wartości x i y obiektu. Dzięki temu wstawiamy go w odpowiednie miejsce na gameBoard. Potem nadajemy mu klasę snake, dzięki czemu go renderujemy, a potem każdy z tych divów jest dodawany do htmla (gameBoard.appendChild). Zerujemy gameBoard.innerHTML żeby ogon węża znikał w każdej iteracji, czyli żeby ruszał się cały, a nie rozciągał.
 
 function renderSnake(){
@@ -213,7 +200,7 @@ function renderSnake(){
     })  
 }
 
-// 03. UPDATING SNAKE /////////////////////////////////////////////////////
+// 07. UPDATING SNAKE /////////////////////////////////////////////////////
 // Tworzę petlę for, w której snakeBody[i] oznacza przedostatni obiekt tablicy snakeBody, a snakeBody[i+1] - ostatni. Przypisując ostatniemu przedostatni przy każdej iteracji ostatni element "przesuwa się" o jedną pozycję do przodu. Przypisanie musi tworzyć duplikat obiektu ({...object}), a nie przypisywać dokładnie ten sam obiekt, żeby uniknąć problemu z referance. 
 
 function updateSnake(){
@@ -242,7 +229,7 @@ function updateSnake(){
 
 }
 
-// 04. USER DIRECTIONS ////////////////////////////////////////////////////////
+// 08. USER DIRECTIONS ////////////////////////////////////////////////////////
 // Tak jak wyjaśnione powyżej aktualizujemy obiekt userDirections po każdym wciśnięciu strzałki o zadaną wartość x/y, a następnie przekazujemy obiekt z nowymi wartościami do funkcji updateSnake. Poprzez obiekt lastUserDirections sprawdzamy czy zadany ruch jest dozwolony (np. jeśli ostatnim ruchem był ruch w kierunku y to nie możemy znów wykonać ruchu w kierunku y). Przy każdym wywołaniu getUserDirections() aktualizujemy obiekt lastUserDirections.
 
 let userDirections = { x: 0, y: 0};
@@ -274,7 +261,7 @@ function getUserDirections(){
     return userDirections
 }
 
-// 05. VICTIM RENDER & UPDATE ///////////////////////////////////////////////////
+// 09. VICTIM RENDER & UPDATE ///////////////////////////////////////////////////
 
 function renderVictim(){
     const victimCell = document.createElement('div');
@@ -284,7 +271,7 @@ function renderVictim(){
     gameBoard.appendChild(victimCell)
 }
 
-// 06. EATING VICTIM /////////////////////////////////////////////////////////
+// 10. EATING VICTIM /////////////////////////////////////////////////////////
 // Funkcja eatVictim() zarządza całym zdarzeniem, gdy którakolwiek komórka snake'a pokrywa się z komórką victima. W tym celu sprawdza warunek z funkcją onSnake(). Jeśli jest ona prawdziwa, wywołuje dwa wyrażenia - funkcję expandSnake, która wydłuża snake'a o zadany EXPANSION_RATE oraz przypisanie obiektowi victim nowych współrzędnych. Funkcja onSnake() sprawdza czy którakolwiek komórka snake'a pokrywa się z victimem. Posługuje się przy tym metodą .some wywołaną na arrayu snakeBody. Funkcja wywołuje kolejną funkcję equalPositions, która ma za argumenty komórkę snake'a i argument przywołany w onSnake (czyli w naszym przypadku będzie tam victim) i wówczas porównuje współrzędne x i y obu tych argumentów. Funkcje onSnake i equalPositions zwracają boolean, także funkcja główna będzie wywołana tylko jeśli oba będą spełnione. Główna funkcja eatVictim jest przekazana do funkcji update(), która jest iterowana co określony przez SNAKE_SPEED czas.
 
 let victimScore = 0;
@@ -308,7 +295,7 @@ function equalPositions(pos1, pos2){
     return pos1.x === pos2.x && pos1.y === pos2.y;
 }
 
-// 07. ADDING NEW SNAKE CELLS ////////////////////////////////////////////////////
+// 11. ADDING NEW SNAKE CELLS ////////////////////////////////////////////////////
 // Przy dodawaniu nowych komórek snake'a kluczową zmienną będzie newCells, do której przy każdym wywołaniu expandSnake będzie dodawana ilość komórek równa zadanej EXPANSION_RATE. Funkcją przekazaną do jednej z głównych funkcji - updateSnake() jest addCells, która pushuje nowy obiekt do  arraya snakeBody tyle razy ile wynosi wartość newCells, czyli tyle ile EXPANSION_RATE. Po każdym dodaniu nowych komórek, po loop break zmienna newCells jest zerowana, inaczej snake wydłużałby się w nieskończoność.
 
 let newCells = 0;
@@ -324,7 +311,7 @@ function addCells(){
     newCells = 0;
 }
 
-// 08. RANDOMIZE VICTIM POSITION /////////////////////////////////////////////////
+// 12. RANDOMIZE VICTIM POSITION /////////////////////////////////////////////////
 // Funkcja Math.random() zwraca wartość od 0 do 0,99999 dlatego mnożę ją przez 21
 function randomVictimPosition(){
     return {
@@ -333,7 +320,7 @@ function randomVictimPosition(){
     }
 }
 
-// 09. VERYFYING IF GAME IS OVER ////////////////////////////////////////////////
+// 13. VERYFYING IF GAME IS OVER ////////////////////////////////////////////////
 
 function checkGameOver() {
     if (form.wallsoff.checked === true){
@@ -341,7 +328,6 @@ function checkGameOver() {
     } else {
         gameOver = outsideGrid(snakeBody[0]) || snakeIntersection()
     }
-    
 }
 
 function outsideGrid(position){
@@ -354,10 +340,3 @@ function outsideGrid(position){
 function snakeIntersection(){
     return onSnake(snakeBody[0], { ignoreHead: true })
 }
-
-// Co chcę dodać sam później: 0.uprość kod podobnie jak zrobiłeś z randomVictimPosition 1.wyświetlacz wyników i zapisywanie wyników, możliwość resetowania gry itd 2.przy włączaniu gry wpisywanie nazwy użytkownika i potem zapisywanie wyników poszczególnych użytkowników 3.możliwość ustawiania SNAKE_SPEED i EXPANSION_RATE na stronie 4.włączanie i wyłączanie ścian 5.teleport 
-
-
-
-
-
